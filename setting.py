763d68be69c73b638e767a4b17aa57ed7a7d5e77
chungSungMin <taky0315@naver.com>
2025-01-31 13:37:52 +0900

    🤡 Create docstring and clean code
    maek docstirng and clean code for data setting

--- setting.py
+++ setting.py
@@ -1,14 +1,21 @@
-import pandas as pd
-import logging
-
-
 def data_setting(config):
+    """
+    데이터를 불러오고, 모델 학습 및 최적화에 필요한 설정 값들을 추출하는 함수입니다.
 
-    # 데이터 경로 설정 및 타겟 설정
+    Args:
+        config (dict): 설정 정보를 담고 있는 딕셔너리.
+
+    Returns:
+        data(pd.DataFrame): 불러온 데이터
+        target(str): 타깃 변수 이름
+        fixed_feature(list): 최적화에서 제외될 피처 목록
+        selected_quality(str): 선택된 모델 품질 옵션
+        time_to_train(int): 모델 학습 시간(초) 제한
+        n_trials(int): 최적화 시도 횟수
+    """
     data_path = config.get('data_path')
     target = config.get('target')
 
-    # 데이터 불러오기
     try:
         data = pd.read_csv(data_path)
         logging.info(f"Data loaded from {data_path}")
@@ -16,40 +23,18 @@ def data_setting(config):
         logging.error(f"Failed to load data: {e}")
         return
 
-    # # 자동으로 고정할 피처 감지
-    # try:
-    #     auto_fixed_features = auto_determine_fixed_features(data, target)
-    #     logging.info(f"Automatically determined fixed features: {auto_fixed_features}")
-    # except Exception as e:
-    #     logging.error(f"Failed to determine fixed features: {e}")
-    #     return
-
-    # # 사용자 지정 피처 고정 추가
-    # user_fixed_features = config.get('user_fixed_features', [])
-    # fixed_features = list(set(auto_fixed_features + user_fixed_features))
-    # logging.info(f"Fixed Features (Not to be optimized): {fixed_features}")
-
-
     user_fixed_features = config.get('user_fixed_features', [])
     fixed_features = list(set(user_fixed_features))
     logging.info(f"Fixed Features (Not to be optimized): {fixed_features}")
 
-    # 모델 품질 매핑 및 선택
-    quality_map = config.get('quality_map', {
-        0: "best_quality",
-        1: "high_quality",
-        2: "good_quality",
-        3: "medium_quality"
-    })
+    quality_map = config.get('quality_map')
     selected_quality = quality_map.get(config.get('selected_quality', 0), "Invalid selection")
     if selected_quality == "Invalid selection":
         logging.error("Invalid quality selection. Please choose a valid option from quality_map.")
         return
     logging.info(f"Selected quality: {selected_quality}")
 
-    # 모델 학습 시간 및 최적화 시도 횟수 설정
     time_to_train = config.get('time_to_train', 500)
     n_trials = config.get('n_trials', 100)
-
 
     return data, target, fixed_features, selected_quality, time_to_train, n_trials
