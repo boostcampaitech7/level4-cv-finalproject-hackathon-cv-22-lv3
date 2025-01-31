b486ab6f41372c36b0f522b044caaa33fb538b28
chungSungMin <taky0315@naver.com>
2025-01-31 13:44:44 +0900

    🚧 Data preprocessing pipline
    concat the preprocessing code in 21 line

--- preprocessing/data_preprocessing.py
+++ preprocessing/data_preprocessing.py
@@ -3,6 +3,19 @@ from simple_preprocessing import simple_preprocessing, add_grad_column
 import logging
 
 def base_preprocessing(config, data, target):
+    """데이터를 통해서 기본적으로 ( target 값,  (min,max)값, target의 클래스, 기본적인 전처리를 진행합니다.)
+
+    Args:
+        config (dict): 설정 정보를 담고 있는 딕셔너리 
+        data (pd.DataFrame): 불러온 데이터 ㅡ레임
+        target (_type_): 최적화 하고자 하는 Feature
+
+    Returns:
+        task(str): 어떤 task를 진행할지
+        data(pd.DataFrame): 기본 전처리가 진행된 데이터셋
+        direction(str) : Minimize OR Maximize
+        target_class(str) : 최적화 할 class가 어떤 종류인지
+    """
     try:
         task = determine_problem_type(data, target)
         data = simple_preprocessing(data, task)
