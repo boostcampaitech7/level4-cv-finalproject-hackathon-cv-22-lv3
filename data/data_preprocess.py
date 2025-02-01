7324376d33dbe31c80b31a122819547a232d548e
bigstar <2002bigstar@gmail.com>
2025-02-01 07:51:33 +0000

    🎨 Update process_features & add _date_features
    날짜 데이터 처리 메서드 추가

--- data/data_preprocess.py
+++ data/data_preprocess.py
@@ -1,5 +1,6 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+from datetime import datetime
 from omegaconf import OmegaConf
 from sklearn.impute import SimpleImputer, KNNImputer
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler, StandardScaler, PowerTransformer
@@ -36,6 +37,7 @@ class DataPreprocessor:
             if feature["type"] == "Numeric":
                 z_scores = (self.data[col] - feature["mean"]) / feature["std"]
                 self.data = self.data[np.abs(z_scores) <= z_thresh]
+        
         return self.data
 
     def handle_missing_values(self, col, strategy="mean"):
@@ -51,6 +53,7 @@ class DataPreprocessor:
         '''
         feature = self.data_info[col]
         missing_ratio = feature["p_missing"]            
+        
         if missing_ratio < 0.03:
             self.data = self.data.dropna(subset=[col])  # 결측치 비율이 3% 미만이면 행 삭제
         else:
@@ -66,11 +69,12 @@ class DataPreprocessor:
                 raise ValueError("Invalid strategy. Choose from 'mean', 'median', 'mode', 'knn'.")
             
             self.data[[col]] = imputer.fit_transform(self.data[[col]])
+        
         return self.data
 
     def process_column(self, col):
         '''
-        summary: 각 열의 특성(type, n_distinct)을 기반으로 적절한 전처리를 수행합니다.
+        summary: 각 열의 특성(type)을 기반으로 적절한 전처리를 수행합니다.
 
         args:
             col (str): 처리할 열 이름.
@@ -81,90 +85,62 @@ class DataPreprocessor:
         feature = self.data_info[col]
         feature_type = feature['type']
 
-        if feature_type == 'Categorical':
-            self._categorical_features(col, feature['n_distinct'])
-        
-        elif feature_type == 'Numeric':
-            self._numeric_features(col, feature['min'], abs(feature['skewness']))
-        
-        elif feature_type == 'Boolean':
-            self._boolean_features(col)
-        
-        return self.data
+        n_distinct = feature['n_distinct']
 
-
-    def _categorical_features(self, col, n_distinct):
-        '''
-        summary: 범주형 변수를 처리하는 함수. 
-             고유값 개수(n_distinct)에 따라 OneHotEncoder 또는 LabelEncoder를 적용합니다.
-
-        args:
-            col (str): 처리할 열 이름.
-            n_distinct (int): 해당 열의 고유값 개수.
-
-        return:
-            None: 데이터프레임을 직접 수정하며, 인코더 정보를 self.decoders에 저장합니다.
-        '''
         if n_distinct == 1:
             self.data.drop(columns=[col], inplace=True)
             return
+
+        if feature_type == 'Categorical' or feature_type == 'Boolean':
+            self._categorical_features(col)
         
-        if n_distinct <= 2:
-            # OneHotEncoder 적용
-            ohe = OneHotEncoder(sparse_output=False)
-            label = self.data[col].values.reshape(-1, 1)
-            one_hot_encoded = ohe.fit_transform(label)
-    
-            # 새로운 컬럼 이름 생성
-            feature_names = ohe.get_feature_names_out([col])
+        elif feature_type == 'Numeric':
+            self._numeric_features(col)
+        
+        elif feature_type == 'DateTime':
+            self._date_features(col)
+                
+        return self.data
 
-            # 원-핫 인코딩 결과를 DataFrame으로 변환
-            one_hot_df = pd.DataFrame(one_hot_encoded, columns=feature_names, index=self.data.index)
-
-            # 기존 컬럼 삭제 후 새로운 데이터프레임 병합
-            self.data.drop(columns=[col], inplace=True)
-            self.data[feature_names] = one_hot_df
-            
-            # 인코더 정보 저장
-            self.decoders[col] = {
-                'encoder': ohe,
-                'feature_names' : feature_names
-            }
-
-        else:
-            # Label Encoding 적용
-            le = LabelEncoder()
-            self.data[col] = le.fit_transform(self.data[col])
-            
-            # 인코더 정보 저장
-            self.decoders[col] = {
-                'encoder': le,
-            }
-
-    def _numeric_features(self, col, min_value, skewness):
+    def _categorical_features(self, col):
         '''
-        summary: 수치형 변수를 처리하는 함수. 
-                데이터의 왜도(skewness)와 최소값(min_value)에 따라 적절한 변환을 적용합니다.
+        summary: 범주형 변수를 처리하는 함수. 
+                 고유값 개수(n_distinct)에 따라 OneHotEncoder 또는 LabelEncoder를 적용합니다.
 
         args:
             col (str): 처리할 열 이름.
-            min_value (float): 해당 열의 최소값.
-            skewness (float): 해당 열의 왜도 (1 이상이면 비대칭 분포로 간주).
 
         return:
             None: 데이터프레임을 직접 수정하며, 인코더 정보를 self.decoders에 저장합니다.
         '''
+        # Label Encoding 적용
+        le = LabelEncoder()
+        self.data[col] = le.fit_transform(self.data[col])
+        
+        # 인코더 정보 저장
+        self.decoders[col] = {
+            'encoder': le,
+            }
+
+    def _numeric_features(self, col):
+        '''
+        summary: 수치형 변수를 처리하는 함수. 
+                데이터의 왜도(skewness)에 따라 적절한 변환을 적용합니다.
+
+        args:
+            col (str): 처리할 열 이름.
+
+        return:
+            None: 데이터프레임을 직접 수정하며, 인코더 정보를 self.decoders에 저장합니다.
+        '''
+        skewness = abs(self.data_info[col]['skewness'])
         transformer = None
 
         if skewness >= 1:
-            if min_value <= 0:
-                transformer = PowerTransformer(method='yeo-johnson')
-            else:
-                transformer = PowerTransformer(method='box-cox')
-
+            transformer = PowerTransformer(method='yeo-johnson')
         elif skewness < 0.5:
             transformer = StandardScaler()
-
+        
         if transformer:
             self.data[[col]] = transformer.fit_transform(self.data[[col]])
         
@@ -174,24 +150,33 @@ class DataPreprocessor:
 
         self.decoders[col] = {
             "encoder" : [min_max_scaler, transformer]
-        }
+            }
 
-    def _boolean_features(self, col):
+    def _date_features(self, col):
         '''
-        summary: 불리언 변수를 처리하는 함수. 
-                True/False 값을 0/1로 변환하기 위해 LabelEncoder를 적용합니다.
+        summary: DateTime 열을 년, 월, 일로 나누어 개별 숫자형 변수로 변환합니다. 
+             이를 통해 모델이 날짜 정보를 학습할 수 있도록 돕습니다.
 
         args:
             col (str): 처리할 열 이름.
 
         return:
             None: 데이터프레임을 직접 수정하며, 인코더 정보를 self.decoders에 저장합니다.
-        '''
-        le = LabelEncoder()
-        self.data[col] = le.fit_transform(self.data[col])
+        '''        
+        # 현재 연도 가져오기 (기본값 설정용)
+        current_year = datetime.now().year
+        
+        self.data[col] = pd.to_datetime(self.data[col], errors='coerce')
+
+        self.data[f'{col}_year'] = self.data[col].dt.year.fillna(current_year).astype(int)
+        self.data[f'{col}_month'] = self.data[col].dt.month.fillna(1).astype(int)  # 월이 없으면 1월
+        self.data[f'{col}_day'] = self.data[col].dt.day.fillna(1).astype(int)  # 일이 없으면 1일
+        
+        self.data.drop(columns=[col], inplace=True)
+        
         self.decoders[col] = {
-            'encoder': le
-        }
+            'columns': [f'{col}_year', f'{col}_month', f'{col}_day']
+            }
 
     def process_features(self, strategy="mean"):
         '''
@@ -200,17 +185,13 @@ class DataPreprocessor:
         return: 
             pd.DataFrame: 전처리가 완료된 데이터프레임.
         '''
-        processed_frames = []
-
-        for col in self.data.columns:
+        original_columns = list(self.data.columns)
+        
+        for col in original_columns:
             self.handle_missing_values(col, strategy)
-            processed_col = self.process_column(col)
-            if processed_col is not None:
-                processed_frames.append(processed_col)
-
-        # 전처리된 모든 열 병합
-        return pd.concat(processed_frames, axis=1)
-    
+            processed_df = self.process_column(col)
+            
+        return processed_df    
 
     def decode(self, col):
         '''
@@ -223,24 +204,15 @@ class DataPreprocessor:
             pd.DataFrame: 디코딩이 완료된 데이터프레임.
         '''
         if col not in self.decoders:
-            raise ValueError(f"{col}에 대한 인코더 정보가 없습니다.")
+            return self.data
 
         feature = self.data_info[col]
         feature_type = feature['type']
-        n_distinct = feature['n_distinct']
 
         encoder_info = self.decoders[col]
-        encoder = encoder_info['encoder']
+        encoder = encoder_info['encoder'] if 'encoder' in encoder_info else encoder_info['columns']
 
-        if (feature_type == 'Categorical') and (n_distinct <= 2):
-            feature_names = encoder_info['feature_names']
-            one_hot_values = self.data[feature_names].values
-            original_labels = encoder.inverse_transform(one_hot_values)
-
-            self.data[col] = original_labels.squeeze()
-            self.data.drop(columns=feature_names, inplace=True)
-
-        elif (feature_type == 'Categorical' and n_distinct > 2) or (feature_type == 'Boolean'):
+        if (feature_type == 'Categorical') or (feature_type == 'Boolean'):
             self.data[col] = encoder.inverse_transform(self.data[col])
         
         elif feature_type == 'Numeric':
@@ -250,17 +222,29 @@ class DataPreprocessor:
             if transfomer:
                 self.data[[col]] = transfomer.inverse_transform(self.data[[col]])
         
-        return self.data
+        elif feature_type == 'DateTime':
+            year_col, month_col, day_col = encoder_info['columns']
+            
+            self.data[col] = pd.to_datetime(
+                self.data[[year_col, month_col, day_col]].astype(str).agg('-'.join, axis=1),
+                errors='coerce'
+            )
+    
+            # 변환된 연, 월, 일 컬럼 삭제 (선택 사항)
+            self.data.drop(columns=encoder_info['columns'], inplace=True)
 
+        return self.data
+    
 
 if __name__ == "__main__":
     # CSV 파일 로드
-    df = pd.read_csv("/data/ephemeral/home/data/mat2.csv")
+    df = pd.read_csv("/data/ephemeral/home/data/time/DailyDelhiClimateTrain.csv")
     print("초기 데이터 정보:")
+    print(df.head())
     print(df.info())
 
     # 데이터 EDA 정보를 JSON에서 로드
-    data_info = OmegaConf.load('/data/ephemeral/home/level4-cv-finalproject-hackathon-cv-22-lv3/eda/student_math/filtered_result.json')
+    data_info = OmegaConf.load('/data/ephemeral/home/level4-cv-finalproject-hackathon-cv-22-lv3/eda/time_data/filtered_result.json')
 
     # 데이터 전처리 객체 생성
     if "Unnamed: 0" in df.columns:
