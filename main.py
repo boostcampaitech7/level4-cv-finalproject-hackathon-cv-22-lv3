f5c81a7f365c5d0c051b66d9f83aae5626486979
bigstar <2002bigstar@gmail.com>
2025-02-05 20:56:26 +0000

    💡 Update main pipeline
    코드 간소화 및 중복 제거 #19

--- main.py
+++ main.py
@@ -10,12 +10,10 @@ from utils.user_input import save_config_to_json, add_config_to_json
 from user_input.login import create_userinfo
 from user_input.data_upload import upload_dataset
 from data.data_info import get_json
-from user_input.base_setting import user_base_setting
+from user_input.setting_input import user_base_setting, model_base_setting, base_optimize_setting
 from utils.determine_feature import feature_selection
-from user_input.model_setting import model_base_setting
 from utils.analysis_feature import identify_categorical_features
 from utils.determine_feature import categorical_feature
-from user_input.optimization_setting import base_optimize_setting
 
 def main_pipline():
     """
@@ -30,21 +28,21 @@ def main_pipline():
     json_file_path, eda_html_path = get_json(user_name, user_email, data_path) # 업로드된 데이터 EDA 진행 및 반환 가능
 
     features = visualization_feature(json_file_path) # 환경변수, 제어변수, 타겟변수, 갯수 설정
-    target_feature, controllable_feature, opt_range, necessary_feature, limited_feature =  user_base_setting(features)
+    user_settings =  user_base_setting(features)
 
-    file_path = save_config_to_json(user_name, user_email, data_path, target_feature, controllable_feature, opt_range, necessary_feature, limited_feature) # config 파일 설정
+    file_path, config = save_config_to_json(user_name, user_email, data_path, user_settings) # config 파일 설정
 
     ctr_feature, env_feature = feature_selection(file_path, len(features)) # 제어변수, 환경변수 따로 가져오기
     data_frame = make_filtered_data(file_path) # 사용할 변수들만 포함된 데이터프레임
 
-    task = determine_problem_type(data_frame, target_feature) # task 설정
-    train_time, model_quality = model_base_setting() # train_time, model_quality 입력
-    add_config_to_json(file_path, task, train_time, model_quality) # json에 추가로 작성하기
+    task = determine_problem_type(data_frame, config) # task 설정
+    model_config = model_base_setting(task) # train_time, model_quality 입력
+    config = add_config_to_json(file_path, model_config) # json에 추가로 작성하기
 
     merged_file_path, processed_df = base_preprocessing(data_frame, file_path) # 데이터 전처리
 
     # 모델 학습을 위한 train_to_time, quality, task 필요
-    model, test_df = train_model(data_frame, task, target_feature, model_quality, train_time)
+    model, test_df = train_model(processed_df, config)
 
 
     # 카테고리컬 feature를 구분한다.
@@ -52,12 +50,13 @@ def main_pipline():
     logging.info("Starting feature optimization to maximize the target variable...")
 
 
-    direction, n_trials, target_class = base_optimize_setting(merged_file_path, task)
+    opt_config = base_optimize_setting(task)
+    config = add_config_to_json(merged_file_path, opt_config)
 
     # 최적화를 진행한다.
     comparison_df, original_prediction, optimized_prediction_value = feature_optimize(
-        data_frame, task, target_feature, direction, n_trials, target_class, test_df,
-        model, categorical_features, env_feature )
+        data_frame, task, config, test_df,
+        model, categorical_features, env_feature)
     
     return comparison_df, original_prediction, optimized_prediction_value
 
